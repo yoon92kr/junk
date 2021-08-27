5961ab1efda4f007539f63b389bfda5ae56d6b72
yoon92kr <yoon92_kr@naver.com>
2021-08-28 07:41:41 +0900

    Calculator2 교육받은 class 및 생성자를 응용해 버전2 Refactor

--- calculator.java
+++ calculator.java
@@ -1,36 +1,43 @@
 package PersonalRepository;
 import java.util.Scanner;
 
-public class calculator {
+class Calculator {
+	double sum;
+	double minus;
+	double multiply;
+	double division;
+	double worng = 0;
 	
-	static double calculator (String a, double b, double c) {
+	String operator;
+	double first;
+	double sceond;
+	
+	Calculator(String operator, double first, double sceond) {
+		this.operator = operator;
+		this.first = first;
+		this.sceond = sceond;
+	}
+	
+	double run() {
 		
-		
-		double sum;
-		double minus;
-		double multiply;
-		double division;
-		double worng = 0;
-		
-		if (a.equals("+")==true) {
+		if (operator.equals("+")==true) {
 			
-			sum = b+c;
-			
-		return sum; 
+			sum = first+sceond;
+			return sum; 
 		}
 		
-		else if ( a.equals("-")==true) {
-			minus = b-c;
+		else if (operator.equals("-")==true) {
+			minus = first-sceond;
 			return minus;
 		}
 		
-		else if ( a.equals("*")==true) {
-			multiply = b*c;
+		else if (operator.equals("*")==true) {
+			multiply = first*sceond;
 			return multiply;
 		}
 		
-		else if (a.equals("/")==true) {
-			division = b/c;
+		else if (operator.equals("/")==true) {
+			division = first/sceond;
 			return division;
 			
 		}
@@ -40,10 +47,11 @@ public class calculator {
 	}
 	
 	public static void main(String[] args) {
+		
 	Scanner sn = new Scanner(System.in);
 	String operator;
-	double first, scoend;
-	double result;
+	double first, sceond;
+	
 	System.out.println("Enter Operator (+,-,*,/)");
 	operator = sn.next();
 	
@@ -51,10 +59,10 @@ public class calculator {
 	first = sn.nextDouble();
 	
 	System.out.println("Enter scoend number");
-	scoend = sn.nextDouble();
+	sceond = sn.nextDouble();
 	
-	result = calculator(operator, first, scoend);
-	System.out.println(first+""+operator+""+scoend+" = " +result);
+	Calculator inn = new Calculator(operator, first, sceond);
+	System.out.println(first+""+operator+""+sceond+" = " +inn.run());
 	
 	
 	sn.close();
