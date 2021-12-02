06439e5790bfb6c40ad3f4cb69d2c97d73670470
yoon92kr <73813647+yoon92kr@users.noreply.github.com>
2021-12-02 18:07:27 +0900

    2021.12.02
    ▶ sql 프로시저 일부 수정

--- board/board.sql
+++ board/board.sql
@@ -53,3 +53,23 @@ CREATE TABLE `t_imagefile` (
   KEY `articleNO_idx` (`articleNO`),
   CONSTRAINT `articleNO` FOREIGN KEY (`articleNO`) REFERENCES `t_board` (`articleno`)
 ) ;
+
+create table sequences(
+sequence_name varchar(20),
+count_no int unsigned)
+# unsigned : 음수를 사용하지 않고, 양수로 범위를 확대
+engine = innodb;	#사용 엔진 설정
+
+DROP PROCEDURE IF EXISTS create_sequence;
+
+DELIMITER //
+create procedure create_sequence (in the_name text)	#파라미터 매개변수로 the_name를 수령, 자료형 text
+modifies sql data	#함수/프로시져가 데이터베이스에 저장된 튜플을 수정할 수 있다는 선언(DDL)
+deterministic	#비교 등 함수/프로시져 호출시 연산마다 함수/프로시져의 반환값이 변경될수 있기에 재호출할것인지(not deterministic)  고정할것인지(deterministic) 정의)
+
+begin
+delete from sequences where sequence_name = the_name;
+insert into sequences values(the_name, 400);	#오라클의 START WITH[시작 수]와 동일한 역할, 시작값을 지정
+end//
+
+DELIMITER ;
