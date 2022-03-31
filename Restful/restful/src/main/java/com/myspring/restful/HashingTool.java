9283df37181a78ae291b826a571acc6f086c01e8
DESKTOP-BCCAT56\arom <yoon92_kr@naver.com>
2022-03-31 14:21:26 +0900

    2022.03.31 hash / salt 구현

---
+++ Restful/restful/src/main/java/com/myspring/restful/HashingTool.java
@@ -0,0 +1,47 @@
+package com.myspring.restful;
+
+import java.security.MessageDigest;
+
+public class HashingTool {
+
+	public String setParam(byte[] target, String salt) throws Exception {
+
+		String result = hashing(target, salt);
+
+		return result;
+	}
+
+	private String hashing(byte[] target, String salt) throws Exception {
+
+		String result = "";
+
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+		for (int i = 0; i < salt.length(); i++) {
+			md.update(target);
+			target = md.digest();
+			result = byteToString(target);
+		}
+
+		result += salt;
+
+		md.update(target);
+		target = md.digest();
+		result = byteToString(target);
+
+		System.out.println(result);
+
+		return result;
+	}
+
+	private String byteToString(byte[] target) {
+
+		StringBuilder sb = new StringBuilder();
+		for (byte a : target) {
+			sb.append(String.format("%02x", a));
+		}
+		return sb.toString();
+
+	}
+
+}
--- Restful/restful/src/main/java/com/myspring/restful/MainController.java
+++ Restful/restful/src/main/java/com/myspring/restful/MainController.java
@@ -1,17 +1,50 @@
 package com.myspring.restful;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-	
+
 	@ResponseBody
-	@RequestMapping("/test.do")
-	public String home() {
+	@RequestMapping(value = "/hashing.do", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
+	public String home(HttpServletRequest request, HttpServletResponse response, @RequestParam HashMap<String, Object> param) {
 		
-		return "home";
+		
+		System.out.printf("IP : %s 의 클라이언트가 요청을 보냈습니다. %n", request.getRemoteAddr());
+
+		HashingTool tool = new HashingTool();
+		String result = "";
+		String userID = "TestID";
+
+		String password = (String) param.get("pw");
+		byte[] byteParam = password.getBytes();
+		String salt = "mordern" + userID + "balanco";
+
+		try {
+			result = tool.setParam(byteParam, salt);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		String target = "481e7746ae29dda46bc2abab03b5066b50d7bfde725959a993fd4b3abbebdf09";
+
+		if(result.equals(target)) {
+			result = "일치";
+		}
+		else {
+			result = "불일치";
+		}
+		
+		return result;
 	}
-	
+
 }
