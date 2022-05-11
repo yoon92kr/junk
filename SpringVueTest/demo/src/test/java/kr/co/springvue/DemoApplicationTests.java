f5d5f247116e5c5f13f95074e0bf17e09cdae2aa
yoon92kr <yoon92_kr@naver.com>
2022-05-12 08:26:05 +0900

    Vue Test

---
+++ SpringVueTest/demo/src/test/java/kr/co/springvue/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.springvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
