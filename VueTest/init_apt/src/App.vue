8eeeacd55f82f234bcbcd333857df11ba6f20b43
yoon92kr <yoon92_kr@naver.com>
2022-05-31 08:46:01 +0900

    Vue Project

---
+++ VueTest/init_apt/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HeaderNV />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import HeaderNV from "./components/HeaderNV.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    HeaderNV,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueTest/init_apt/src/components/HeaderNV.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>HeaderNV</h1>
+</template>
+<script>
+export default {
+  name: "HeaderNV",
+};
+</script>
