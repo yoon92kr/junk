52b987449b0510e37560fb0dba53985bda85982c
yoon92kr <yoon92_kr@naver.com>
2022-07-08 23:49:31 +0900

    Update main.js

--- VueTest/init_apt/src/main.js
+++ VueTest/init_apt/src/main.js
@@ -3,7 +3,6 @@ import App from "@/App.vue";
 import router from "@/router/index.js";
 import CoreuiVue from "@coreui/vue";
 import PrimeVue from "primevue/config";
-
 import { TroisJSVuePlugin } from "troisjs";
 
 /* CSS */
@@ -11,7 +10,7 @@ import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "@coreui/coreui/dist/css/coreui.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+ 
 const app = createApp(App);
 app.use(router);
 app.use(PrimeVue);
